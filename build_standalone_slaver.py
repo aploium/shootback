ff783e46ceb2ab48853d9c897b7205a44ce4cca4
Aploium <i@z.codes>
2017-02-02 22:44:18 +0800

    alpha4

---
+++ build_standalone_slaver.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+def build_standalone_slaver(outfile_name="slaver_standalone.py"):
+    import os
+    import shutil
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    output_fullpath = os.path.join(base_path, outfile_name)
+
+    if os.path.exists(output_fullpath):
+        ch = input("target file: " + output_fullpath + " already exist, overwrite it?\n(y/N)")
+        if ch not in ("y", "Y", "yes"):
+            print("user abort")
+            return None
+        else:
+            os.remove(output_fullpath)
+
+    shutil.copy(
+        os.path.join(base_path, "common_func.py"),
+        os.path.join(base_path, outfile_name, )
+    )
+    with open("slaver.py", "r", encoding='utf-8') as fr:
+        with open(outfile_name, "a", encoding="utf-8") as fw:
+            fw.write(fr.read())
+
+    print("generate complete!\noutput file:", output_fullpath)
+
+
+if __name__ == '__main__':
+    build_standalone_slaver()
