60efa40458afa253b1e333ca691d18c2e51fd2d0
Aploium <i@z.codes>
2017-02-04 14:29:57 +0800

    add comments for common_func.py

--- common_func.py
+++ common_func.py
@@ -13,20 +13,33 @@ import threading
 import traceback
 import functools
 
-log = logging.getLogger(__name__)
+try:
+    # for pycharm type hinting
+    from typing import Union, Callable
+except:
+    pass
 
-# slaver_pool = collections.deque()
-# working_pool = {}
-# spare_slaver_pool = {}
-
-RECV_BUFFER_SIZE = 2 ** 14  # 16384 bytes
+# socket recv buffer, 16384 bytes
+RECV_BUFFER_SIZE = 2 ** 14
+# default secretkey, use -k/--secretkey to change
 SECRET_KEY = "shootback"
-SPARE_SLAVER_TTL = 600  # 600s
-INTERNAL_VERSION = 0x0002
-__version__ = (2, 1, 0, INTERNAL_VERSION)
+# how long a SPARE slaver would keep 600s
+# once slaver received an heart-beat package from master,
+#   the TTL would be reset. And heart-beat delay is less than TTL,
+#   so, theoretically, spare slaver never timeout,
+#   except network failure
+# notice: working slaver would NEVER timeout
+SPARE_SLAVER_TTL = 600
+# internal program version, appears in CtrlPkg
+INTERNAL_VERSION = 0x0003
+__version__ = (2, 1, 1, INTERNAL_VERSION)
+
+# just a logger
+log = logging.getLogger(__name__)
 
 
 def version_info():
+    """get program version for human. eg: "2.1.0-r2" """
     return "{}.{}.{}-r{}".format(*__version__)
 
 
@@ -38,10 +51,12 @@ def configure_logging(level):
 
 
 def fmt_addr(socket):
+    """(host, int(port)) --> "host:port" """
     return "{}:{}".format(*socket)
 
 
 def split_host(x):
+    """ "host:port" --> (host, int(port))"""
     try:
         host, port = x.split(":")
         port = int(port)
@@ -54,31 +69,63 @@ def split_host(x):
 
 
 def try_close(closable):
+    """try close something
+
+    same as
+        try:
+            connection.close()
+        except:
+            pass
+    """
     try:
         closable.close()
     except:
         pass
 
 
-def select_recv(conn, size, timeout=None):
+def select_recv(conn, buff_size, timeout=None):
+    """add timeout for socket.recv()
+    :type conn: socket.SocketType
+    :type buff_size: int
+    :type timeout: float
+    :rtype: Union[bytes, None]
+    """
     rlist, _, elist = select.select([conn], [], [conn], timeout)
     if not rlist or elist:
-        # 超时或出错
+        # timeout or exception occurs
         return None
 
-    return conn.recv(size)
+    return conn.recv(buff_size)
 
 
 class SocketBridge:
+    """
+    transfer data between sockets
+    """
+
     def __init__(self):
-        self.conn_rd = set()
-        self.map = {}
-        self.callbacks = {}
+        self.conn_rd = set()  # record readable-sockets
+        self.map = {}  # record sockets pairs
+        self.callbacks = {}  # record callbacks
 
     def add_conn_pair(self, conn1, conn2, callback=None):
-        self.conn_rd.update((conn1, conn2))
+        """
+        transfer anything between two sockets
+
+        :type conn1: socket.SocketType
+        :type conn2: socket.SocketType
+        :param callback: callback in connection finish
+        :type callback: Callable
+        """
+        # mark as readable
+        self.conn_rd.add(conn1)
+        self.conn_rd.add(conn2)
+
+        # record sockets pairs
         self.map[conn1] = conn2
         self.map[conn2] = conn1
+
+        # record callback
         if callback is not None:
             self.callbacks[conn1] = callback
 
@@ -99,63 +146,105 @@ class SocketBridge:
                 ))
 
     def _start(self):
+        # memoryview act as an recv buffer
+        # refer https://docs.python.org/3/library/stdtypes.html#memoryview
         buff = memoryview(bytearray(RECV_BUFFER_SIZE))
         while True:
             if not self.conn_rd:
+                # sleep if there is no connections
                 time.sleep(0.1)
                 continue
 
+            # blocks until there is socket(s) ready for .recv
+            # notice: sockets which were closed by remote,
+            #   are also regarded as read-ready by select()
             r, w, e = select.select(self.conn_rd, [], [])
 
-            for s in r:
+            for s in r:  # iter every read-ready or closed sockets
                 try:
+                    # here, we use .recv_into() instead of .recv()
+                    #   recv data directly into the pre-allocated buffer
+                    #   to avoid many unnecessary malloc()
+                    # see https://docs.python.org/3/library/socket.html#socket.socket.recv_into
                     rec_len = s.recv_into(buff, RECV_BUFFER_SIZE)
                 except:
-                    self._rd_close(s)
+                    # unable to read, in most cases, it's due to socket close
+                    self._rd_shutdown(s)
                     continue
 
                 if not rec_len:
-                    self._rd_close(s)
+                    # read zero size, closed or shutdowned socket
+                    self._rd_shutdown(s)
                     continue
+
                 try:
+                    # send data, we use `buff[:rec_len]` slice because
+                    #   only the front of buff is filled
                     self.map[s].send(buff[:rec_len])
                 except:
+                    # unable to send, close connection
+                    self._wr_shutdown(s)
                     log.debug(e)
                     continue
 
-    def _rd_close(self, conn, once=False):
+    def _rd_shutdown(self, conn, once=False):
+        """action when connection should be read-shutdown
+        :type conn: socket.SocketType
+        """
         if conn in self.conn_rd:
             self.conn_rd.remove(conn)
+
         try:
             conn.shutdown(socket.SHUT_RD)
         except:
             pass
 
-        if not once:
-            self._wr_close(self.map[conn], True)
+        if not once:  # use the `once` param to avoid infinite loop
+            # if a socket is rd_shutdowned, then it's
+            #   pair should be wr_shutdown.
+            self._wr_shutdown(self.map[conn], True)
 
         if self.map.get(conn) not in self.conn_rd:
+            # if both two connection pair was rd-shutdowned,
+            #   this pair sockets are regarded to be completed
+            #   so we gonna close them
             self._terminate(conn)
 
-    def _wr_close(self, conn, once=False):
-        if not once:
-            self._rd_close(self.map.get(conn), True)
+    def _wr_shutdown(self, conn, once=False):
+        """action when connection should be write-shutdown
+        :type conn: socket.SocketType
+        """
+        if not once:  # use the `once` param to avoid infinite loop
+            # if a socket is wr_shutdowned, then it's
+            #   pair should be rd_shutdown.
+            self._rd_shutdown(self.map.get(conn), True)
+
         try:
             conn.shutdown(socket.SHUT_WR)
         except:
             pass
 
     def _terminate(self, conn):
-        try_close(conn)
+        """terminate a sockets pair (two socket)
+        :type conn: socket.SocketType
+        :param conn: any one of the sockets pair
+        """
+        try_close(conn)  # close the first socket
+
+        # ------ close and clean the mapped socket, if exist ------
         if conn in self.map:
             _mapped_conn = self.map[conn]
             try_close(_mapped_conn)
             if _mapped_conn in self.map:
                 del self.map[_mapped_conn]
         else:
-            _mapped_conn = None
-        del self.map[conn]
+            _mapped_conn = None  # just a fallback
 
+        del self.map[conn]  # clean the first socket
+
+        # ------ callback --------
+        # because we are not sure which socket are assigned to callback,
+        #   so we should try both
         if conn in self.callbacks:
             self.callbacks[conn]()
             del self.callbacks[conn]
@@ -166,6 +255,12 @@ class SocketBridge:
 
 class CtrlPkg:
     """
+    Control Packages of shootback, not completed yet
+    current we have: handshake and heartbeat
+
+    NOTICE: If you are non-Chinese reader,
+        please contact me for the following Chinese comment's translation
+        http://github.com/aploium
 
     控制包结构 总长64bytes      CtrlPkg.FORMAT_PKG
     使用 big-endian
@@ -204,29 +299,38 @@ class CtrlPkg:
     """
     PACKAGE_SIZE = 2 ** 6  # 64 bytes
 
-    # 密匙的CRC32
+    # CRC32 for SECRET_KEY and Reversed(SECRET_KEY)
     SECRET_KEY_CRC32 = binascii.crc32(SECRET_KEY.encode('utf-8')) & 0xffffffff
     SECRET_KEY_REVERSED_CRC32 = binascii.crc32(SECRET_KEY[::-1].encode('utf-8')) & 0xffffffff
 
     # Package Type
-    PTYPE_HS_S2M = -1  # Handshake Slaver to Master
-    PTYPE_HEART_BEAT = 0  # 心跳
-    PTYPE_HS_M2S = +1  # Handshake Master to Slaver
+    PTYPE_HS_S2M = -1  # handshake pkg, slaver to master
+    PTYPE_HEART_BEAT = 0  # heart beat pkg
+    PTYPE_HS_M2S = +1  # handshake pkg, Master to Slaver
 
-    # formats
-    FORMAT_PKG = "!c b H 20x 40s"
-    FORMATS_DATA = {
-        PTYPE_HS_S2M: "I 36x",
-        PTYPE_HEART_BEAT: "40x",
-        PTYPE_HS_M2S: "I 36x",
+    TYPE_NAME_MAP = {
+        PTYPE_HS_S2M: "PTYPE_HS_S2M",
+        PTYPE_HEART_BEAT: "PTYPE_HEART_BEAT",
+        PTYPE_HS_M2S: "PTYPE_HS_M2S",
     }
 
-    _PREBUILT_PKG = {}
+    # formats
+    # see https://docs.python.org/3/library/struct.html#format-characters
+    #   for format syntax
+    FORMAT_PKG = "!c b H 20x 40s"
+    FORMATS_DATA = {
+        PTYPE_HS_S2M: "!I 36x",
+        PTYPE_HEART_BEAT: "!40x",
+        PTYPE_HS_M2S: "!I 36x",
+    }
+
+    _cache_prebuilt_pkg = {}  # cache
 
     def __init__(self, pkg_ver=b'\x01', pkg_type=0,
                  prgm_ver=INTERNAL_VERSION, data=(),
                  raw=None,
                  ):
+        """do not call this directly, use `CtrlPkg.pbuild_*` instead"""
         self.pkg_ver = pkg_ver
         self.pkg_type = pkg_type
         self.prgm_ver = prgm_ver
@@ -236,6 +340,22 @@ class CtrlPkg:
         else:
             self._build_bytes()
 
+    @property
+    def type_name(self):
+        """返回人类可读的包类型"""
+        return self.TYPE_NAME_MAP.get(self.pkg_type, "TypeUnknown")
+
+    def __str__(self):
+        return """pkg_ver: {} pkg_type:{} prgm_ver:{} data:{}""".format(
+            self.pkg_ver,
+            self.type_name,
+            self.prgm_ver,
+            self.data,
+        )
+
+    def __repr__(self):
+        return self.__str__()
+
     def _build_bytes(self):
         self.raw = struct.pack(
             self.FORMAT_PKG,
@@ -244,6 +364,15 @@ class CtrlPkg:
             self.prgm_ver,
             self.data_encode(self.pkg_type, self.data),
         )
+
+    @classmethod
+    def _prebuilt_pkg(cls, pkg_type, fallback):
+        """act as lru_cache"""
+        if pkg_type not in cls._cache_prebuilt_pkg:
+            pkg = fallback(force_rebuilt=True)
+            cls._cache_prebuilt_pkg[pkg_type] = pkg
+
+        return cls._cache_prebuilt_pkg[pkg_type]
 
     @classmethod
     def recalc_crc32(cls):
@@ -279,6 +408,14 @@ class CtrlPkg:
 
     @classmethod
     def decode_only(cls, raw):
+        """
+        decode raw bytes to CtrlPkg instance, no verify
+        use .decode_verify() if you also want verify
+
+        :param raw: raw bytes content of package
+        :type raw: bytes
+        :rtype: CtrlPkg
+        """
         if len(raw) != cls.PACKAGE_SIZE:
             raise ValueError("content size should be {}, but {}".format(
                 cls.PACKAGE_SIZE, len(raw)
@@ -295,6 +432,16 @@ class CtrlPkg:
 
     @classmethod
     def decode_verify(cls, raw, pkg_type=None):
+        """decode and verify a package
+        :param raw: raw bytes content of package
+        :type raw: bytes
+        :param pkg_type: assert this package's type,
+            if type not match, would be marked as wrong
+        :type pkg_type: int
+
+        :rtype: CtrlPkg, bool
+        :return: tuple(CtrlPkg, is_it_a_valid_package)
+        """
         try:
             pkg = cls.decode_only(raw)
         except:
@@ -303,15 +450,8 @@ class CtrlPkg:
             return pkg, pkg.verify(pkg_type=pkg_type)
 
     @classmethod
-    def _prebuilt_pkg(cls, pkg_type, fallback):
-        if pkg_type not in cls._PREBUILT_PKG:
-            pkg = fallback(force_rebuilt=True)
-            cls._PREBUILT_PKG[pkg_type] = pkg
-
-        return cls._PREBUILT_PKG[pkg_type]
-
-    @classmethod
     def pbuild_hs_m2s(cls, force_rebuilt=False):
+        """pkg build: Handshake Master to Slaver"""
         # because py27 do not have functools.lru_cache, so we must write our own
         if force_rebuilt:
             return cls(
@@ -323,6 +463,7 @@ class CtrlPkg:
 
     @classmethod
     def pbuild_hs_s2m(cls, force_rebuilt=False):
+        """pkg build: Handshake Slaver to Master"""
         if force_rebuilt:
             return cls(
                 pkg_type=cls.PTYPE_HS_S2M,
@@ -333,20 +474,10 @@ class CtrlPkg:
 
     @classmethod
     def pbuild_heart_beat(cls, force_rebuilt=False):
+        """pkg build: Heart Beat Package"""
         if force_rebuilt:
             return cls(
                 pkg_type=cls.PTYPE_HEART_BEAT,
             )
         else:
             return cls._prebuilt_pkg(cls.PTYPE_HEART_BEAT, cls.pbuild_heart_beat)
-
-    def __str__(self):
-        return """CtrlPkg<pkg_ver: {} pkg_type:{} prgm_ver:{} data:{}>""".format(
-            self.pkg_ver,
-            self.pkg_type,
-            self.prgm_ver,
-            self.data,
-        )
-
-    def __repr__(self):
-        return self.__str__()
